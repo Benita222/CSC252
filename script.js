f599c513d914909cfeb468662397e192b68b6d71
comerade029 <okoyepeterchinaza@gmail.com>
2024-09-12 19:26:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+ //  1a Declare and Initialize an Array
+
+
+ let sb= [1,3,4,6,6];
+ console.log(sb);
+ 
+ 
+           //   1b 2 Dimensional Array
+   
+ 
+ let bb=[ 
+     [2,3,4,7],
+     [3,5,7,8,],
+     [3,4,6,7,],         
+ ];
+ console.log(bb);
+ 
+   
+           // 1c Concatenate an Array
+     
+ let cc=[2,3,6,7];
+ let cb= cc.join(",");
+ console.log(cb);
+   
+           // 1d Array Transversal
+   
+ let vb=[3,4,6,7];
+ for (let index = 0; index < vb.length; index++) {
+ };
+ console.log(vb);
